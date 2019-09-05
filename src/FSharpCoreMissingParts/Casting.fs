5bc1c6e3e5b20d6615c57a7810324fa22bc2e59a
Bang Jun-young <bang.junyoung@gmail.com>
2019-09-06 01:29:31 +0900

    FSharpCoreMissingParts.Casting 네임스페이스를 열어야 제네릭 캐스팅을 사용할 수 있도록 변경

--- src/FSharpCoreMissingParts/Casting.fs
+++ src/FSharpCoreMissingParts/Casting.fs
@@ -23,7 +23,7 @@
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-namespace FSharpCoreMissingParts
+namespace FSharpCoreMissingParts.Casting
 
 [<AutoOpen>]
 module Casting =
